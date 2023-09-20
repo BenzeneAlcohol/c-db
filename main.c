bf37de32616b80f6db7af565dc037173af9ff467
BenzeneAlcohol <muthuku37@gmail.com>
2023-09-20 15:40:37 +0530

    Added better comments and code cleanup

--- main.c
+++ main.c
@@ -12,24 +12,25 @@ struct InputBuffer_t {
 
 typedef struct InputBuffer_t InputBuffer; //InputBuffer = struct InputBuffer_t
 
-ssize_t getline(char **lineptr, size_t *n, FILE *stream); 
-
 //lineptr = Pointer to *buffer of input_buffer (which itself is a pointer, hence pointer of pointers)
 //size_t is a datatype similar to int, but gaurantees that it can hold the size of the object (in our case, the input length/buffer length)
+ssize_t getline(char **lineptr, size_t *n, FILE *stream); //Returns the number of characters read
 
+//Reading the input from the user
 void read_input(InputBuffer* input_buffer) {
-  ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+  ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin); //Number of bytes read is returned (Including newLine)
 
   if (bytes_read <= 0) {
     printf("Error reading input\n");
     exit(EXIT_FAILURE);
   }
 
-  // Ignore trailing newline
-  input_buffer->input_length = bytes_read - 1;
-  input_buffer->buffer[bytes_read - 1] = 0;
+  input_buffer->input_length = bytes_read - 1; //Reducing the length of newLine
+  input_buffer->buffer[bytes_read - 1] = 0; //Replacing \n with \0
 }
 
+//Returns an InputBuffer* object
+//Basic initialization of the object in the memory
 InputBuffer* new_input_buffer() {
   InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
   input_buffer->buffer = NULL;
@@ -44,7 +45,7 @@ void print_prompt() {
 }
 
 int main(int argc, char* argv[]) {
-  InputBuffer* input_buffer = new_input_buffer();
+  InputBuffer* input_buffer = new_input_buffer(); //initializing the structure
   while (true) {
     print_prompt();
     read_input(input_buffer);
