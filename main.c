78856f956dfebdc3cb38376504cf3f29d74cedbb
BenzeneAlcohol <muthuku37@gmail.com>
2023-09-20 15:56:28 +0530

    Better code formatting

--- main.c
+++ main.c
@@ -5,34 +5,34 @@
 #include <string.h>
 
 struct InputBuffer_t {
-  char* buffer; //String of your inpur command
+  char *buffer; // String of your inpur command
   size_t buffer_length;
-  ssize_t input_length; //Length of that string  (signed buffer)
+  ssize_t input_length; // Length of that string  (signed buffer)
 };
 
-typedef struct InputBuffer_t InputBuffer; //InputBuffer = struct InputBuffer_t
+typedef struct InputBuffer_t InputBuffer; // InputBuffer = struct InputBuffer_t
 
-//lineptr = Pointer to *buffer of input_buffer (which itself is a pointer, hence pointer of pointers)
-//size_t is a datatype similar to int, but gaurantees that it can hold the size of the object (in our case, the input length/buffer length)
-ssize_t getline(char **lineptr, size_t *n, FILE *stream); //Returns the number of characters read
+// lineptr = Pointer to *buffer of input_buffer (which itself is a pointer, hence pointer of pointers)
+// size_t is a datatype similar to int, but gaurantees that it can hold the size of the object (in our case, the input length/buffer length)
+ssize_t getline(char **lineptr, size_t *n, FILE *stream); // Returns the number of characters read
 
-//Reading the input from the user
-void read_input(InputBuffer* input_buffer) {
-  ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin); //Number of bytes read is returned (Including newLine)
+// Reading the input from the user
+void read_input(InputBuffer *input_buffer) {
+  ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin); // Number of bytes read is returned (Including newLine)
 
   if (bytes_read <= 0) {
     printf("Error reading input\n");
     exit(EXIT_FAILURE);
   }
 
-  input_buffer->input_length = bytes_read - 1; //Reducing the length of newLine
-  input_buffer->buffer[bytes_read - 1] = 0; //Replacing \n with \0
+  input_buffer->input_length = bytes_read - 1; // Reducing the length of newLine
+  input_buffer->buffer[bytes_read - 1] = 0;    // Replacing \n with \0
 }
 
-//Returns an InputBuffer* object
-//Basic initialization of the object in the memory
-InputBuffer* new_input_buffer() {
-  InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+// Returns an InputBuffer* object
+// Basic initialization of the object in the memory
+InputBuffer *new_input_buffer() {
+  InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
   input_buffer->buffer = NULL;
   input_buffer->buffer_length = 0;
   input_buffer->input_length = 0;
@@ -40,17 +40,23 @@ InputBuffer* new_input_buffer() {
   return input_buffer;
 }
 
-void print_prompt() { 
-    printf("db > "); 
+void print_prompt() {
+  printf("db > ");
 }
 
-int main(int argc, char* argv[]) {
-  InputBuffer* input_buffer = new_input_buffer(); //initializing the structure
+void free_input_buffer(InputBuffer *input_buffer) {
+  free(input_buffer->buffer);
+  free(input_buffer);
+}
+
+int main(int argc, char *argv[]) {
+  InputBuffer *input_buffer = new_input_buffer(); // initializing the structure
   while (true) {
     print_prompt();
     read_input(input_buffer);
 
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
+      free_input_buffer(input_buffer);
       exit(EXIT_SUCCESS);
     } else {
       printf("Unrecognized command '%s'.\n", input_buffer->buffer);
